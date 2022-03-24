fa06d484db1c9933afb61d56463b4cd3f9335324
Gabriel Pizarro <gpizarro@javaman.net>
2022-03-24 10:19:33 -0700

    Switch to one-page UI

--- UnrealVRLauncher/MainWindow.xaml.cs
+++ UnrealVRLauncher/MainWindow.xaml.cs
@@ -1,39 +1,10 @@
-﻿using System;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Navigation;
-
-namespace UnrealVRLauncher
+﻿namespace UnrealVRLauncher
 {
     public sealed partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void NavigationView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
-        {
-            
-            Type pageType;
-            var item = (NavigationViewItem) sender.SelectedItem;
-            if (item == GeneralItem)
-            {
-                pageType = typeof(GeneralPage);
-            }
-            else if (item == ProfileItem)
-            {
-                pageType = typeof(ProfilePage);
-            }
-            else
-            {
-                pageType = typeof(AboutPage);
-            }
-            var navOptions = new FrameNavigationOptions
-            {
-                TransitionInfoOverride = args.RecommendedNavigationTransitionInfo
-            };
-            ContentFrame.NavigateToType(pageType, null, navOptions);
-            sender.Header = item.Content;
         }
     }
 }
