7f4ed85abed07f778b00aa0ff06d1aa326306923
Gabriel Pizarro <gpizarro@javaman.net>
2022-03-25 07:05:34 -0700

    Begin UI functionality, add native DLL interop

--- UnrealVRLauncher/MainWindow.xaml.cs
+++ UnrealVRLauncher/MainWindow.xaml.cs
@@ -1,10 +1,59 @@
-﻿namespace UnrealVRLauncher
+﻿using Microsoft.UI.Xaml.Controls;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+
+namespace UnrealVRLauncher
 {
-    public sealed partial class MainWindow
+    public sealed partial class MainWindow : INotifyPropertyChanged
     {
         public MainWindow()
         {
             InitializeComponent();
+            Profiles.Add(new ProfileModel("Ghostrunner", "Ghostrunner-1.json", "C:\\Ghostrunner\\Shipping.exe", "-dx11", 1.1f));
+            Profiles.Add(new ProfileModel("Borderlands 3", "Borderlands 3-1.json", "C:\\Borderlands 3\\Shipping.exe", "-dx11", 1.1f));
+            Profiles.Add(new ProfileModel("Astroneer", "Astroneer-1.json", "C:\\Astroneer\\Shipping.exe", "-dx11", 1.1f));
+        }
+
+        public ObservableCollection<ProfileModel> Profiles = new();
+        public ProfileModel Profile;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var comboBox = sender as ComboBox;
+            Profile = comboBox.SelectedItem as ProfileModel;
+            NotifyPropertyChanged(nameof(Profile));
+        }
+    }
+
+    public class ProfileModel
+    {
+        public ProfileModel(string name, string filename, string shippingExe, string commandLineArgs, float cmUnitsScale)
+        {
+            Name = name;
+            Filename = filename;
+            ShippingExe = shippingExe;
+            CommandLineArgs = commandLineArgs;
+            CmUnitsScale = cmUnitsScale;
+        }
+
+        public string Name { get; set; } = "Untitled";
+        public string Filename { get; set; } = "";
+        public string ShippingExe { get; set; } = "";
+        public string CommandLineArgs { get; set; } = "";
+        public float CmUnitsScale { get; set; } = 1.0f;
+
+        public string FormatCmUnitsScale()
+        {
+            return CmUnitsScale.ToString("F3", CultureInfo.InvariantCulture);
         }
     }
 }
---
+++ UnrealVRLauncher/Native.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace UnrealVRLauncher
+{
+    /**
+     * DLL functions, translated to C#
+     * 
+     * Remember to use out/ref for applicable parameters
+     * 
+     * Credits:
+     * - http://www.pinvoke.net/ : VirtualAllocEx, WriteProcessMemory, CreateRemoteThread
+     * - https://stackoverflow.com/a/24339722/6909078 : CreateProcess, CloseHandle
+     */
+    public class Native
+    {
+        /** Get dwSize using Marshal.SizeOf(typeof(TYPE)) */
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        static extern IntPtr VirtualAllocEx(
+            IntPtr hProcess, 
+            IntPtr lpAddress,
+            uint dwSize, 
+            uint flAllocationType, 
+            uint flProtect
+        );
+
+        [Flags]
+        public enum AllocationType
+        {
+            Commit = 0x1000,
+            Reserve = 0x2000,
+            Decommit = 0x4000,
+            Release = 0x8000,
+            Reset = 0x80000,
+            Physical = 0x400000,
+            TopDown = 0x100000,
+            WriteWatch = 0x200000,
+            LargePages = 0x20000000
+        }
+
+        [Flags]
+        public enum MemoryProtection
+        {
+            Execute = 0x10,
+            ExecuteRead = 0x20,
+            ExecuteReadWrite = 0x40,
+            ExecuteWriteCopy = 0x80,
+            NoAccess = 0x01,
+            ReadOnly = 0x02,
+            ReadWrite = 0x04,
+            WriteCopy = 0x08,
+            GuardModifierflag = 0x100,
+            NoCacheModifierflag = 0x200,
+            WriteCombineModifierflag = 0x400
+        }
+
+        [DllImport("kernel32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool CreateProcess(
+            string lpApplicationName, 
+            string lpCommandLine,
+            ref SECURITY_ATTRIBUTES lpProcessAttributes,
+            ref SECURITY_ATTRIBUTES lpThreadAttributes,
+            bool bInheritHandles,
+            uint dwCreationFlags,
+            IntPtr lpEnvironment,
+            string lpCurrentDirectory,
+            [In] ref STARTUPINFOEX lpStartupInfo,
+            out PROCESS_INFORMATION lpProcessInformation
+        );
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        struct STARTUPINFOEX
+        {
+            public STARTUPINFO StartupInfo;
+            public IntPtr lpAttributeList;
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        struct STARTUPINFO
+        {
+            public Int32 cb;
+            public string lpReserved;
+            public string lpDesktop;
+            public string lpTitle;
+            public Int32 dwX;
+            public Int32 dwY;
+            public Int32 dwXSize;
+            public Int32 dwYSize;
+            public Int32 dwXCountChars;
+            public Int32 dwYCountChars;
+            public Int32 dwFillAttribute;
+            public Int32 dwFlags;
+            public Int16 wShowWindow;
+            public Int16 cbReserved2;
+            public IntPtr lpReserved2;
+            public IntPtr hStdInput;
+            public IntPtr hStdOutput;
+            public IntPtr hStdError;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct PROCESS_INFORMATION
+        {
+            public IntPtr hProcess;
+            public IntPtr hThread;
+            public int dwProcessId;
+            public int dwThreadId;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SECURITY_ATTRIBUTES
+        {
+            public int nLength;
+            public IntPtr lpSecurityDescriptor;
+            public int bInheritHandle;
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool CloseHandle(IntPtr hObject);
+
+        [DllImport("kernel32.dll")]
+        static extern bool WriteProcessMemory(
+             IntPtr hProcess,
+             IntPtr lpBaseAddress,
+             byte[] lpBuffer,
+             Int32 nSize,
+             out IntPtr lpNumberOfBytesWritten
+        );
+
+        /** Example at https://docs.microsoft.com/en-us/archive/blogs/jmstall/using-createremotethread-from-c */
+        [DllImport("kernel32.dll")]
+        static extern IntPtr CreateRemoteThread(
+            IntPtr hProcess,
+            IntPtr lpThreadAttributes,
+            uint dwStackSize, 
+            IntPtr lpStartAddress,
+            IntPtr lpParameter,
+            uint dwCreationFlags,
+            out IntPtr lpThreadId
+        );
+    }
+}
